fbb93d8080cacb68839e0fa70ee0286c3abe39ba
MinGu Lee <immigration9@MinGuui-MacBook-Pro.local>
2018-01-15 02:40:12 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Udemy - Test Driven Development in Java
---
+++ src/main/java/com/immigration9/udemytdd/Calculator.java
@@ -0,0 +1,33 @@
+package com.immigration9.udemytdd;
+
+import java.math.BigDecimal;
+import java.util.Stack;
+
+public class Calculator {
+
+	// private BigDecimal accumulator = BigDecimal.ZERO;
+	private Stack<BigDecimal> values = new Stack<BigDecimal>();
+	
+	public BigDecimal getAccumulator() {
+		if (values.size() == 0) {
+			return BigDecimal.ZERO;
+		}
+		return values.peek();
+	}
+
+	public void setAccumulator(BigDecimal value) {
+		if (values.size() > 0) {
+			values.pop();
+		}
+		values.push(value);
+	}
+
+	public void enter() {
+		values.push(getAccumulator());
+	}
+
+	public void drop() {
+		values.pop();
+	}
+	
+}
